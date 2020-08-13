96cd6d66a36c3237e30758650fd733ede6dd8bdb
Jim Lamb <jimlamb@hey.com>
2020-08-13 11:57:26 -0400

    Removed contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,65 +56,23 @@ const HomeIndex = () => {
             whichever method you prefer.
           </p>
           <div className="row">
-            <div className="8u 12u$(small)">
-              <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-                <p class="hidden">
-                  <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-                </p>
-                <div className="row uniform 50%">
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="12u">
-                    <textarea
-                      name="message"
-                      id="message"
-                      placeholder="Message"
-                      rows="4"
-                    ></textarea>
-                  </div>
-                </div>
-                <ul className="actions" style={{ marginTop: 30 }}>
-                  <li>
-                    <input type="submit" value="Send Message" />
-                  </li>
-                </ul>
-              </form>
-            </div>
             <div className="4u 12u$(small)">
               <ul className="labeled-icons">
-                <li>
-                  <h3 className="icon fa-home">
-                    <span className="label">Address</span>
-                  </h3>
-                  Cary, NC 27519
-                  <br />
-                  United States
-                </li>
-                <li>
-                  <h3 className="icon fa-mobile">
-                    <span className="label">Phone</span>
-                  </h3>
-                  +1 (919) 475-7244
-                </li>
                 <li>
                   <h3 className="icon fa-envelope-o">
                     <span className="label">Email</span>
                   </h3>
                   <a href="mailto:jimlamb@hey.com">jimlamb@hey.com</a>
+                </li>
+              </ul>
+            </div>
+            <div className="4u 12u$(small)">
+              <ul className="labeled-icons">
+                <li>
+                  <h3 className="icon fa-mobile">
+                    <span className="label">Phone</span>
+                  </h3>
+                  +1 (919) 475-7244
                 </li>
               </ul>
             </div>
